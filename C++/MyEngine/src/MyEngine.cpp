9ec60e1579d09e18d409c7660053116473ad9096
mikhail0090025 <mikhail.ivashchenko.07@gmail.com>
2019-12-15 22:24:40 +0300

    Second commit

---
+++ C++/MyEngine/src/MyEngine.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : MyEngine.cpp
+// Author      : Mikhail Ivashchenko
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+using namespace std;
+#include <iostream>
+//using namespace std;
+
+int main() {
+	//cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	system('pause');
+	return 0;
+}
